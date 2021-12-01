529e42a852e52d4f561348e9bfca69d39512c519
iqrafatimame <iqrafatimame@gmail.com>
2021-12-01 20:08:55 +0500

    Changed the js logic

--- main.js
+++ main.js
@@ -1,99 +1,50 @@
-const todos = [
-    {
-        text: "Take out the trash",
-        isCompleted: false,
-    },
-];
+var todoIndex = 0
+var errorText = document.getElementById("error-text")
+var todoText = document.getElementById("task")
 
-const form = document.getElementById('form');
-form.addEventListener('submit', (e) => {
+const form = document.getElementById('form')
+form.addEventListener('submit', e => {
     e.preventDefault()
-});
-var text = document.getElementById("error-text")
-
-
-function addTodo(e) {
-    const input = document.getElementById("task");
-    if (input.value == "") {
-        setError(task)
-    }
-    else {
-        text.innerHTML = ""
-        task.style.borderColor =  ""
-        todos.push({
-            text: input.value,
-            isCompleted: false
-        })
-    }
-    input.value = "" // clear input field value
-    displayTodos()
-}
-
+})
 
 function setError(){
-    text.innerHTML = "Todo can't be empty"
-    text.style.color = "#ff0000"
-    task.style.borderColor =  "#ff0000"
+    errorText.innerHTML = "Todo can't be empty"
+    errorText.style.color = "#ff0000"
+    todoText.style.borderColor =  "#ff0000"
 }
 
-function toggleTodo(todoIndex) {
-    todos[todoIndex].isCompleted = !todos[todoIndex].isCompleted
-    displayTodos()
-}
-
-function deleteTodo(todoIndex) {
-    todos.splice(todoIndex, 1)
-    displayTodos()
-}
-
-function displayTodos() {
-    const list = document.getElementById("todo-items-list")
-    list.innerHTML = "" // clear todo list before appending todos again
-
-    todos.forEach((todo, todoIndex) => {
-        const listItem = document.createElement("li")
-        listItem.className = "list-items"
-
-        const checkbox = createCheckbox(todo, todoIndex)
-        const text = createTodoText(todo, todoIndex)
-        const deleteButton = createDeleteButton(todo, todoIndex)
-
-        listItem.append(checkbox)
-        listItem.append(text)
-        listItem.append(deleteButton)
-
-        list.append(listItem)
-    })
-}
-
-function createCheckbox(todo, todoIndex) {
-    const checkbox = document.createElement("input")
-    checkbox.type = "checkbox"
-    checkbox.checked = todo.isCompleted
-    checkbox.addEventListener("change", e => {
-        e.preventDefault()
-        toggleTodo(todoIndex)
-    })
-    return checkbox
-}
-
-function createTodoText(todo, todoIndex) {
-    const text = document.createElement("p")
-    text.innerText = todo.text
-    if (todo.isCompleted) {
-        text.classList += "checked"
+function addTodo(){
+    //const task = document.getElementById('task')
+    if(todoText.value == ""){
+        setError()
     }
-    return text
+    else{
+    const todoItem = 
+    `<li id=task${todoIndex}>
+        <div class="list-items">
+            <input type="checkbox" onchange="completedTodo('task${todoIndex}')" />
+            <p class="todo-text">${todoText.value}</p>
+        </div>
+        <i class="fas fa-trash-alt" onclick="deleteTodo('task${todoIndex}')"></i>
+    </li>`
+    document.getElementById('todo-items-list').innerHTML += todoItem    
+    todoIndex++
+
+    todoText.value = ""
+    }
 }
 
-function createDeleteButton(todo, todoIndex) {
-    const deleteButton = document.createElement("i")
-    deleteButton.className = "fas fa-trash-alt"
-    deleteButton.addEventListener("click", e => {
-        e.preventDefault()
-        deleteTodo(todoIndex)
-    })
-    return deleteButton
+function deleteTodo(todoIndex){
+    const todo = document.getElementById(todoIndex)
+    todo.remove()
 }
 
-displayTodos()
+function completedTodo(todoIndex){
+    const todoItem = document.getElementById(todoIndex)
+    
+    if(todoItem.children[0].children[0].checked){
+        todoItem.children[0].children[1].style.textDecoration = "line-through"
+    } else{
+        todoItem.children[0].children[1].style.textDecoration = "none"
+    }
+}
